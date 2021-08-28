18a85a75a5c1094180eed6564687e9001c0995ac
WOOJIN, LEE <pappagallos@kakao.com>
2021-08-29 01:57:34 +0900

    Optional 사용해서 null pointer exception 예외 처리 진행 완료

--- src/main/java/opensource/carelab/apis/user/mapper/IUserMapper.java
+++ src/main/java/opensource/carelab/apis/user/mapper/IUserMapper.java
@@ -4,10 +4,11 @@ import opensource.carelab.apis.user.model.*;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface IUserMapper {
     R_SignUpData insertUser(P_SignUpData params) throws Exception;
     R_IsDuplicateEmail isDuplicateEmail(P_IsDuplicateEmail params) throws Exception;
-    List<R_SelectUser> selectUser(P_SelectUser params) throws Exception;
+    R_SelectUser selectUser(P_SelectUser params) throws Exception;
 }
--- src/main/java/opensource/carelab/apis/user/service/impl/UserService.java
+++ src/main/java/opensource/carelab/apis/user/service/impl/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements IUserService {
@@ -36,37 +37,35 @@ public class UserService implements IUserService {
     @Override
     public Object selectUser(P_SelectUser params) throws Exception {
         // [1] 파라미터로 전달받은 email 데이터를 mapper에게 전달해서 user 정보 가져오기
-        List<R_SelectUser> userInfoList = mapper.selectUser(params);
-        R_SelectUser userInfo;
+        Optional<R_SelectUser> userInfo = Optional.ofNullable(mapper.selectUser(params));
 
-        // [2] 사용자가 입력한 사용자가 데이터베이스 내에 없을 경우
-        if (userInfoList.size() <= 0) {
+        // [2-1] 사용자가 입력한 사용자가 데이터베이스 내에 있을 경우
+        if (userInfo.isPresent()) {
+            // [3] 사용자가 입력한 비밀번호가 데이터베이스에 저장된 BCrypt와 일치할 경우 if문 실행
+            boolean isValid = BCrypt.checkpw(params.getPassword(), userInfo.get().getUserPassword());
+            if (isValid) {
+                // [3-1] JWTTokenProvider를 생성한 뒤
+                JwtTokenProvider provider = new JwtTokenProvider();
+
+                // [3-2] userContext 생성하여 userInfo의 값을 모두 세팅해주고
+                UserContext userContext = new UserContext();
+                userContext.setUserEmail(userInfo.get().getUserEmail());
+                userContext.setUserName(userInfo.get().getUserName());
+                userContext.setUserPhone(userInfo.get().getUserPhone());
+                userContext.setUserGroupCd(userInfo.get().getUserGroupCd());
+                userContext.setUserStatus(userInfo.get().getUserStatus());
+                userContext.setUserLevel(userInfo.get().getUserLevel());
+
+                // [3-3] 생성한 JWT 토큰을 userContext에 세팅
+                userContext.setToken(provider.createToken(userContext));
+
+                return userContext;
+            } else {
+                return "incorrect email or password.";
+            }
+            // [2-2] 사용자가 입력한 사용자가 데이터베이스 내에 없을 경우
+        } else {
             return "not exist user.";
-        } else {
-            userInfo = userInfoList.get(0);
-        }
-
-        // [3] 사용자가 입력한 비밀번호가 데이터베이스에 저장된 BCrypt와 일치할 경우 if문 실행
-        boolean isValid = BCrypt.checkpw(params.getPassword(), userInfo.getUserPassword());
-        if (isValid) {
-            // [3-1] JWTTokenProvider를 생성한 뒤
-            JwtTokenProvider provider = new JwtTokenProvider();
-
-            // [3-2] userContext 생성하여 userInfo의 값을 모두 세팅해주고
-            UserContext userContext = new UserContext();
-            userContext.setUserEmail(userInfo.getUserEmail());
-            userContext.setUserName(userInfo.getUserName());
-            userContext.setUserPhone(userInfo.getUserPhone());
-            userContext.setUserGroupCd(userInfo.getUserGroupCd());
-            userContext.setUserStatus(userInfo.getUserStatus());
-            userContext.setUserLevel(userInfo.getUserLevel());
-
-            // [3-3] 생성한 JWT 토큰을 userContext에 세팅
-            userContext.setToken(provider.createToken(userContext));
-
-            return userContext;
-        } else {
-            return "incorrect email or password.";
         }
     }
 }
